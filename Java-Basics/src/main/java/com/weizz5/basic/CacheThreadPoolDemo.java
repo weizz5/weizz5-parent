a693526013b0ba8c324d48878f3a9ea6b8d497a2
weizz5 <1411591332@qq.com>
2021-03-15 21:15:08 +0800

    [add]:更新

---
+++ Java-Basics/src/main/java/com/weizz5/basic/CacheThreadPoolDemo.java
@@ -0,0 +1,15 @@
+package com.weizz5.basic;
+
+import java.util.concurrent.Executor;
+
+/**
+ * This is Description
+ *
+ * @author weizz5
+ * @date 2020/12/19
+ */
+public class CacheThreadPoolDemo {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Java-Basics/src/main/java/com/weizz5/basic/ThreadPoolTest.java
@@ -0,0 +1,17 @@
+package com.weizz5.basic;
+
+/**
+ * This is Description
+ *
+ * @author weizz5
+ * @date 2020/12/19
+ */
+public class ThreadPoolTest {
+
+    public static void main(String[] args) {
+
+
+    }
+
+
+}
